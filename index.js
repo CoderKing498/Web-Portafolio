87dd4bb995c1a2cc56afddc087b1f866be4cd97d
CoderKing498 <yerlenisarrieta@gmail.com>
2026-04-25 22:22:27 -0500

    LAST TASKS: style.min.css, edit responsive things and Add Javascript interactivities

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Internet is gone")
+console.log("Welcome to my page")
+
+function cambiarColor(){
+    const boton= document.querySelector(".myButton");
+    let color = getRandomColor();
+
+    boton.style.backgroundColor = color;
+}
+
+function getRandomColor() {
+    let caracteres = "01234567890ABCDEF"
+    color = "#";
+    
+    for(let i = 0; i < 6; i++){
+        color += caracteres[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+function ShowHide(){
+    const container = document.querySelector(".container_2");
+    const overlay = document.querySelector(".overlay");
+
+    container.classList.toggle("hidden");
+    overlay.classList.toggle("active");
+}
